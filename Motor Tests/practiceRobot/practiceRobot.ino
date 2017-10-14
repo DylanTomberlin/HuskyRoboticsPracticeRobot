bdd1e39ab6b94e019bf5fa92bb03536dd0fb85ab
save-buffer <warplane@hotmail.com>
2017-10-14 10:58:32 -0700

    Made robot able to move forward, stop, turn, accelerate, and decelerate

--- Motor Tests/practiceRobot/practiceRobot.ino
+++ Motor Tests/practiceRobot/practiceRobot.ino
@@ -3,39 +3,62 @@
 #define BOTH -1
 int voltage = 0;
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
+void setup()
+{
+    Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-    if(Serial.available() > 0){
-    char input = Serial.read();
-    switch(input){
-      case 'w':
-        turnMotor(
-      break;
-      case 'a':
-      break;
-      case 'd':
-      break;
-      case 's':
+void loop()
+{
+    if(Serial.available())
+    {
+	char input = Serial.read();
+	switch(input)
+	{
+	case 'w':
+	    turnMotor(BOTH);
+	    break;
+	case 'a':
+	    turnMotor(RIGHT); //turn left
+	    break;
+	case 'd':
+	    turnMotor(LEFT); //turn right
+	    break;
+	case 's':
+	    voltage = 0;
+	    turnMotor(BOTH);
+	case 'f':
+	    voltage += 50;
+	    voltage = min(voltage, 250);
+	    break;
+	case 'b':
+	    voltage -= 50;
+	    voltage = max(voltage, 0);
+	    break;
+	default:
+	    break;
+	}
     }
-    
-  //delay(500);
-  }
 }
 
-void turnMotor(int motor){
-  
+void turnMotor(int motor)
+{
+    if(motor == -1)
+    {
+	analogWrite(LEFT, voltage);
+	analogWrite(RIGHT, voltage);
+    }
+    else
+    {
+	analogWrite(motor, voltage);
+    }
 }
 
 /*
  * if(temp >= '0' && temp <= '5'){
-      voltage = temp - '0';
-      Serial.println(voltage);
-      analogWrite(11, voltage*51);
-    }
+ voltage = temp - '0';
+ Serial.println(voltage);
+ analogWrite(11, voltage*51);
+ }
  * if(
  */
